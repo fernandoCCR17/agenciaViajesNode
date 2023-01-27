d8c6fe719f87f48f9df43794b511c6ca60d067af
Fernando De la Cruz Ramos <fernandodelacruzramos2001@gmail.com>
2023-01-26 21:18:26 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import router from './routes/index.js';
+import db from './config/db.js';
+
+const app = express();
+
+// Conectar la bd 
+db.authenticate()
+    .then(() => console.log('Base de datos conectada'))
+    .catch(error => console.log(error));
+
+const port = process.env.PORT || 3000;
+
+// Habilitando PUG
+app.set('view engine', 'pug');
+ 
+// Obtener el año actual
+app.use((req, res, next) => {
+    res.locals.year = new Date().getFullYear();
+    res.locals.nombreSitio = "Agencia de Viajes";
+    next();
+});
+
+// Agregar el body parser para leer los datos del formulario
+app.use(express.urlencoded({extended: true}));
+
+// Definir la carpeta publica
+app.use(express.static('public'))
+
+// Agregando al router
+app.use('/', router);
+
+app.listen(port,() => {
+    console.log(`El servidor esta funcionando en el ${port}`);
+})
